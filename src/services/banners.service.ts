392c8822d3d357c7045fc7d914c5c48cf1dee3a8
NamDS <namds@gmail.com>
2025-04-14 19:05:10 +0700

    update banner

--- src/services/banners.service.ts
+++ src/services/banners.service.ts
@@ -1,28 +1,39 @@
-import { api } from '@/lib/api.config'
-import { API_ENDPOINTS, API_URL_WEBCONFIG } from '@/lib/routes/api'
+import { api } from "@/lib/api.config";
+import { API_ENDPOINTS, API_URL_WEBCONFIG } from "@/lib/routes/api";
+import { redirect } from "next/navigation";
 
 const fetchBanners = async () => {
   const res = await api.get<any>(
     `${API_URL_WEBCONFIG}${API_ENDPOINTS.WEBCONFIG.BANNER}`
-  )
-  return res.data
-}
+  );
+  return res.data;
+};
 const updateBanners = async (payload: { imageUrl: string }) => {
   const res = await api.put<any>(
     `${API_URL_WEBCONFIG}${API_ENDPOINTS.WEBCONFIG.BANNER}`,
     payload
-  )
-  return res
-}
+  );
+  return res;
+};
 const addBanners = async (payload: { imageUrl: string }) => {
-  const res = await api.put<any>(
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  const res = await fetch(
     `${API_URL_WEBCONFIG}${API_ENDPOINTS.WEBCONFIG.BANNER}`,
-    payload
-  )
-  return res
-}
+    { method: "POST", body: JSON.stringify(payload), headers: myHeaders }
+  );
+  return res;
+};
+const deleteBanners = async (id: string): Promise<void> => {
+  const res = await fetch(
+    `${API_URL_WEBCONFIG}${API_ENDPOINTS.WEBCONFIG.BANNER}/${id}`,
+    { method: "DELETE" }
+  );
+  console.log(res);
+};
 export default {
   fetchBanners,
   addBanners,
-  updateBanners
-}
+  updateBanners,
+  deleteBanners,
+};
